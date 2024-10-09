50f8c9cfdb89edc1ede3e3e43bca2fdd3b526f4f
vikasyadav01234 <yadav2911vikas@gmail.com>
2024-10-09 19:48:47 +0530

    "Added updateProfile function to render user data, and darkModeProperties and lightModeProperties functions to switch between dark and light modes."

--- script.js
+++ script.js
@@ -82,3 +82,85 @@ function getUserData(gitUrl) {
       throw error;
     });
 }
+
+//RENDER
+function updateProfile(data) {
+  if (data.message !== "Not Found") {
+    noresults.style.display = "none";
+    function checkNull(param1, param2) {
+      if (param1 === "" || param1 === null) {
+        param2.style.opacity = 0.5;
+        param2.previousElementSibling.style.opacity = 0.5;
+        return false;
+      } else {
+        return true;
+      }
+    }
+    avatar.src = `${data.avatar_url}`;
+    userName.innerText = data.name === null ? data.login : data.name;
+    user.innerText = `@${data.login}`;
+    user.href = `${data.html_url}`;
+    datesegments = data.created_at.split("T").shift().split("-");
+    date.innerText = `Joined ${datesegments[2]} ${
+      months[datesegments[1] - 1]
+    } ${datesegments[0]}`;
+    bio.innerText =
+      data.bio == null ? "This profile has no bio" : `${data.bio}`;
+    repos.innerText = `${data.public_repos}`;
+    followers.innerText = `${data.followers}`;
+    following.innerText = `${data.following}`;
+    user_location.innerText = checkNull(data.location, user_location)
+      ? data.location
+      : "Not Available";
+    page.innerText = checkNull(data.blog, page) ? data.blog : "Not Available";
+    page.href = checkNull(data.blog, page) ? data.blog : "#";
+    twitter.innerText = checkNull(data.twitter_username, twitter)
+      ? data.twitter_username
+      : "Not Available";
+    twitter.href = checkNull(data.twitter_username, twitter)
+      ? `https://twitter.com/${data.twitter_username}`
+      : "#";
+    company.innerText = checkNull(data.company, company)
+      ? data.company
+      : "Not Available";
+    searchbar.classList.toggle("active");
+    profilecontainer.classList.toggle("active");
+  } else {
+    noresults.style.display = "block";
+  }
+}
+
+//SWITCH TO DARK MODE - activateDarkMode()
+function darkModeProperties() {
+  root.setProperty("--lm-bg", "#141D2F");
+  root.setProperty("--lm-bg-content", "#1E2A47");
+  root.setProperty("--lm-text", "white");
+  root.setProperty("--lm-text-alt", "white");
+  root.setProperty("--lm-shadow-xl", "rgba(70,88,109,0.15)");
+  modetext.innerText = "LIGHT";
+  modeicon.src = "./assets/images/sun-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(1000%)");
+  darkMode = true;
+  console.log("darkmode changed to " + darkMode);
+  localStorage.setItem("dark-mode", true);
+  console.log("setting dark mode to false");
+
+  console.log("setting dark mode to true");
+}
+
+//SWITCH TO LIGHT MODE - activateLightMode()
+function lightModeProperties() {
+  root.setProperty("--lm-bg", "#F6F8FF");
+  root.setProperty("--lm-bg-content", "#FEFEFE");
+  root.setProperty("--lm-text", "#4B6A9B");
+  root.setProperty("--lm-text-alt", "#2B3442");
+  root.setProperty("--lm-shadow-xl", "rgba(70, 88, 109, 0.25)");
+  modetext.innerText = "DARK";
+  modeicon.src = "./assets/images/moon-icon.svg";
+  root.setProperty("--lm-icon-bg", "brightness(100%)");
+  darkMode = false;
+  console.log("darkmode changed to " + darkMode);
+
+  localStorage.setItem("dark-mode", false);
+  console.log("setting dark mode to false");
+}
